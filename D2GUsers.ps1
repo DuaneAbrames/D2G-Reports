a5a20a5aa5a7512a5ef327259ba95942b9b1c30a
Duane Abrames <duane@spaztic.net>
2026-04-10 10:33:34 -0400

    Initial Commit

---
+++ D2GUsers.ps1
@@ -0,0 +1,133 @@
+[CmdletBinding()]
+Param()
+#$debug = $true
+Import-Module ActiveDirectory
+$scriptDir = $MyInvocation.MyCommand.Source.Replace($MyInvocation.MyCommand.Name, '')
+if (-not (Test-Path "$scriptDir\Reports")) {
+	New-Item -ItemType Directory -Path "$scriptDir\Reports" | Out-Null
+}
+
+$domain = $((Get-ADDomain).Name)
+
+$customer = (Hostname).Split('-')[0]
+if ($customer -like "template") {
+	Exit
+}
+
+$fileDate = Get-Date -Format "yyyy-MM-dd"
+$fileName = "$scriptDir\Reports\Users-$customer-$fileDate.csv"
+
+if ($debug -ne $true -and (Test-Path C:\istools\demoaudit.ps1)) {
+	#This calls the demo audit script, which disables inactive demo accounts.
+	Write-Progress -Status "Checking for inactive demo accounts" -Activity "Preliminary Checks" -PercentComplete -1
+	& C:\istools\demoaudit.ps1
+	Start-Sleep -Seconds 5
+}
+
+
+## New report - looks for SPLA groups
+$groupIndex = 0
+$spla = @()
+$splaIndex = @{}
+$groups = Get-ADGroup -SearchBase "ou=Licensing Security Groups,$((Get-ADDomain).DistinguishedName)" -Filter *
+$splaGroupNames = @()
+foreach ($group in $groups) {
+	$splaGroupNames += $group.Name
+	Write-Progress -Status "$($group.Name)" -Activity "Enumerating SPLA License Groups" -PercentComplete -1
+	$members = @()
+	Get-ADGroupMember -Recursive $group | ForEach-Object { $members += $_.DistinguishedName }
+	$spla += ,$members
+	$splaIndex.Add($group.Name, $groupIndex)
+	$groupIndex++
+}
+$splaGroups = $splaGroupNames | Sort-Object -Unique
+
+$admins = Get-ADGroupMember 'Domain Admins' | Select-Object -ExpandProperty DistinguishedName
+Write-Progress -Status "Please Wait..." -Activity "Gathering the list of users" -PercentComplete -1
+$output = @()
+$users = Get-ADUser -Properties DisplayName, Company, Department, DistinguishedName -Filter { Enabled -eq $true } | Sort-Object Company, DisplayName
+foreach ($user in $users) {
+	
+	if ($admins -contains ($user.DistinguishedName)) {
+		#Skiping Domain Admin.
+	} else {
+		Write-Progress -Status $user.Name -Activity "$customer " -PercentComplete -1
+		$userReport = New-Object PSObject
+		$userReport | Add-Member -Type NoteProperty -Name "Domain" -Value $domain
+		$userReport | Add-Member -Type NoteProperty -Name "Name" -Value $user.DisplayName
+		$userReport | Add-Member -Type NoteProperty -Name "Company" -Value $customer
+		$userReport | Add-Member -Type NoteProperty -Name "Type" -Value ''
+		$userReport | Add-Member -Type NoteProperty -Name "Notes" -Value ''
+		foreach ($splaGroupName in $splaGroups) {
+			$userReport | Add-Member -Type NoteProperty -Name "$splaGroupName" -Value ''
+		}
+		$serviceLevels = 0
+		$demoTestOrService = 0
+		$office = 0
+		$desktop = 0
+		Write-Progress -Status "$($user.Name) ." -Activity "$customer " -PercentComplete -1
+		foreach ($splaIndexEntry in $splaIndex.GetEnumerator()) {
+			$shortName = $splaIndexEntry.Name
+			if (($spla[$splaIndexEntry.Value]) -contains $user.DistinguishedName) {
+				$userReport.($shortName) = 1
+				if ($shortName -like "ServiceLevel*") 
+				{
+					$userReport.Type += $shortName.Replace('ServiceLevel - ', '')
+					$serviceLevels++
+				}
+				if ($shortName -like "SPLA - Office*") 
+				{
+					$office++
+				}
+				if ($shortName -like "*demo*")
+				{
+					$demoTestOrService++
+				}
+				if ($shortName -like "*test*")
+				{
+					$demoTestOrService++
+				}
+				if ($shortName -like "*service account*")
+				{
+					$demoTestOrService++
+				}
+				if ($shortName -like "*Desktops2Go Basic*" -or $shortName -like "*Desktops2Go Standard*" -or $shortName -like "*Desktops2Go Premium*" -or $shortName -like "*Desktops2Go Extreme*" -or $shortName -like "*Desktops2Go VDI*")
+				{
+					$desktop++
+				}
+			}
+		}
+		if ($serviceLevels -ne 1) {
+			$userReport.Notes += "Service Level Error. "
+		}
+		if ($demoTestOrService -gt 0 -and $office -gt 0) {
+			$userReport.Notes += "Service/Test/Demo Account has Office."
+		}
+		if ($desktop -gt 0 -and $office -lt 1) {
+			$userReport.Notes += "Full Desktop Account has No Office."
+		}
+		
+		Write-Progress -Status "$($user.Name) .." -Activity "$customer " -PercentComplete -1
+		$output += $userReport
+	}
+}
+Write-Progress -Status "Sorting..." -Activity "Finishing Up" -PercentComplete -1
+$output = $output | Sort-Object Company,Name
+
+Write-Progress -Status "Writing CSV Report" -Activity "Finishing Up" -PercentComplete -1
+$output | Export-Csv $fileName -NoTypeInformation
+
+
+$mailServer = (Resolve-DnsName -Type MX nettek.com).NameExchange
+
+$recipients = @("purchasing@nettek.com", "dabrames@nettek.com", "jalmeter@nettek.com")
+Start-Sleep -Seconds 3
+if ($debug -eq $true) {
+	$recipients = @("dabrames@nettek.com")
+}
+
+foreach ($recipient in $recipients) {
+	Write-Progress -Status "Sending Email to $recipient" -Activity "Finishing Up" -PercentComplete -1
+	Send-MailMessage -Attachments $fileName -From Administrator@nettek.com -SmtpServer $mailServer -To $recipient -Subject "Desktops2Go Users - $customer"
+	Start-Sleep -Seconds 2
+}
---
+++ demoaudit.ps1
@@ -0,0 +1,95 @@
+[CmdletBinding()]
+Param()
+
+Import-Module ActiveDirectory
+$now = Get-Date -Format d
+
+$disabledUsers = @()
+$warningUsers = @()
+
+$searchBase = "ou=customers,$((Get-ADDomain).DistinguishedName)"
+
+
+$disableDays = 31
+$warningDays = 30
+$demoUsers = Get-ADGroupMember 'ServiceLevel - Demo Account' -Recursive | Get-ADUser
+$demoUsers += Get-ADUser -SearchBase $searchBase -Properties DisplayName, Company, Department, LastLogonTimestamp, WhenCreated -Filter { Enabled -eq $true } | Where-Object { ($_.Company -like "Demo*") -or ($_.DisplayName -like "*demo*") }
+foreach ($demoUser in $demoUsers) {
+	if ($null -eq $demoUser.LastLogonTimestamp)
+	{
+		Write-Verbose "$($demoUser.DisplayName) has never logged in."
+		#creation time is returned as a dateTime.
+		$lastLogonTime = $demoUser.WhenCreated
+	}
+	else {
+		# logon time is in a different format, so 1600 years off.
+		$lastLogonTime = $([datetime]$demoUser.LastLogonTimestamp).AddYears(1600).ToLocalTime()
+	}
+	if ($lastLogonTime -lt $((Get-Date).AddDays(-$disableDays))) {
+		Write-Verbose "$($demoUser.DisplayName) has not logged in in over $disableDays days, disabling account."
+		Set-ADUser $demoUser -Enabled $false -Description "Auto-Disabled by script on $now - last login $lastLogonTime"
+		$disabledUsers += $demoUser
+	}
+	else
+	{
+		if ($lastLogonTime -lt $((Get-Date).AddDays(-$warningDays))) {
+			Write-Verbose "$($demoUser.DisplayName) has not logged in in over $warningDays days, not disabling account YET."
+			#here would go code to email a warning...
+			$warningUsers += $demoUser
+		}
+		else
+	{
+		if ($lastLogonTime -ge $((Get-Date).AddDays(-$warningDays))) {
+			Write-Verbose "$($demoUser.DisplayName) HAS logged in in the last $warningDays days."
+
+		}
+	}
+	}
+}
+
+$disableDays = 7
+$warningDays = 6
+$testGroupMembers = Get-ADGroupMember 'ServiceLevel - Test Account' -Recursive
+foreach ($testGroupMember in $testGroupMembers) {
+	$testUser = Get-ADUser $testGroupMember -Properties DisplayName, LastLogonTimestamp, WhenCreated
+	if ($null -eq $testUser.LastLogonTimestamp)
+	{
+		Write-Verbose "$($testUser.DisplayName) has never logged in."
+		#creation time is returned as a dateTime.
+		$lastLogonTime = $testUser.WhenCreated
+	}
+	else {
+		# logon time is in a different format, so 1600 years off.
+		$lastLogonTime = $([datetime]$testUser.LastLogonTimestamp).AddYears(1600).ToLocalTime()
+	}
+	if ($lastLogonTime -lt $((Get-Date).AddDays(-$disableDays))) {
+		Write-Verbose "$($testUser.DisplayName) has not logged in in over $disableDays days, disabling account."
+		Set-ADUser $testUser -Enabled $false -Description "Auto-Disabled by script on $now - last login $lastLogonTime"
+		$disabledUsers += $testUser
+	}
+	else
+	{
+		if ($lastLogonTime -lt $((Get-Date).AddDays(-$warningDays))) {
+			Write-Verbose "$($testUser.DisplayName) has not logged in in over $warningDays days, not disabling account YET."
+			#here would go code to email a warning...
+			$warningUsers += $testUser
+		}
+		else
+	{
+		if ($lastLogonTime -ge $((Get-Date).AddDays(-$warningDays))) {
+			Write-Verbose "$($testUser.DisplayName) HAS logged in in the last $warningDays days."
+
+		}
+	}
+	}
+}
+
+
+if ($disabledUsers) {
+	Write-Host "======== Disabled ========="
+	$disabledUsers | Format-Table DisplayName -HideTableHeaders
+}
+if ($warningUsers) {
+	Write-Host "======== Warning ========="
+	$warningUsers | Format-Table DisplayName -HideTableHeaders
+}
